af1ff667ee8f94a9966bb5bd5403e7fc5f2c97b3
SorenGroveToft <sorentoft@live.dk>
2015-12-17 12:13:08 +0100

    THE ACCOUNT PROGRAM
    YEEEEEEEEEEEEEEEEEEY

---
+++ AccountProgram/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace AccountProgram
+{
+    class Account
+    {
+        private double balance;
+
+        public Account (double _balance)
+        {
+
+            balance = _balance;
+        }
+        [ContractArgumentValidator]
+        public void deposit(double amount)
+        {
+            if (amount < 0) throw new ArgumentOutOfRangeException("The deposit amount cannot be below zero.");
+            Contract.EndContractBlock();
+            balance += amount;
+        }
+
+
+        [ContractArgumentValidator]
+        public void withdraw(double amount)
+        {
+            if (amount < 0) throw new ArgumentOutOfRangeException("The withdraw amount cannot be below zero.");
+            if (amount > getBalance()) throw new ArgumentOutOfRangeException("The withdraw amount cannot exceed the balance.");
+            Contract.EndContractBlock();
+            balance -= amount;
+        }
+        public double getBalance()
+        {
+            return balance;
+        }
+
+        public void setBalance(double _balance)
+        {
+            balance = _balance;
+        }
+    }
+}
---
+++ AccountProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Your mother");
+            Account derp = new Account(1000);
+            derp.deposit(10000);
+            derp.deposit(-1);
+            derp.withdraw(1000);
+            derp.withdraw(-1);
+            derp.withdraw(9999999);
+            Console.WriteLine(derp.getBalance());
+            while (true) { }
+        }
+    }
+}
